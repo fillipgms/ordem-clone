5b63df9713282a3a52e8378d0d4821624b28514b
Fillip <fillipgms@gmail.com>
2022-07-08 18:24:03 -0300

    update

--- js/atributos.js
+++ js/atributos.js
@@ -1,7 +1,16 @@
 const atributesBtn = document.getElementById("atributes-open")
+const periciasBtn = document.getElementById("pericias-open")
+
+periciasBtn.addEventListener("click", () => {
+    let main = document.getElementById("pericias-main")
+
+    main.classList.toggle("notActive")
+})
 
 atributesBtn.addEventListener("click", () => {
     let main = document.getElementById("atributes-main")
+    let atributes = document.getElementById("atributes")
 
+    atributes.classList.toggle("closed")
     main.classList.toggle("notActive")
 })
--- js/general.js
+++ js/general.js
@@ -1,11 +1,17 @@
 const button = document.querySelectorAll(".open-close")
 const icon = document.querySelectorAll(".arrow-icon")
+const morrendo = document.getElementById("morrendo")
+
+morrendo.addEventListener("click", () => {
+    let kaiser = document.getElementById("kaiser")
+    kaiser.classList.toggle("morto")
+})
+
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', () => {
-        for (let i = 0; i < icon.length; i++) {
-            icon[i].classList.toggle("fa-angle-down")
-            icon[i].classList.toggle("fa-angle-up")
+        for (let i = 0; i < button.length; i++) {
+            icon[i].classList.toggle("closed")
         }
     });
 }
